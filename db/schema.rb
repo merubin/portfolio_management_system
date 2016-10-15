5dda4161bab4414574b83a0835a13b776bb26330
Michael Rubin <michael@rubinsworld.com>
2016-10-15 12:24:42 -0400

    removed email field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161014223815) do
+ActiveRecord::Schema.define(version: 20161015162147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,6 @@ ActiveRecord::Schema.define(version: 20161014223815) do
     t.string   "name"
     t.string   "userid"
     t.string   "password"
-    t.string   "email"
     t.string   "avatar_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
